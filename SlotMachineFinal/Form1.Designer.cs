b1c8d8e69e3ddc35408904239c5c0de5f86d926b
BCBeno <circiubeni@gmail.com>
2025-11-27 22:16:03 +0200

    Commit

---
+++ SlotMachineFinal/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace SlotMachineFinal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnSpin = new System.Windows.Forms.Button();
+            this.numCycles = new System.Windows.Forms.NumericUpDown();
+            this.spinTimer = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numCycles)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSpin
+            // 
+            this.btnSpin.Location = new System.Drawing.Point(129, 181);
+            this.btnSpin.Name = "btnSpin";
+            this.btnSpin.Size = new System.Drawing.Size(517, 193);
+            this.btnSpin.TabIndex = 0;
+            this.btnSpin.Text = "Trage!";
+            this.btnSpin.UseVisualStyleBackColor = true;
+            this.btnSpin.Click += new System.EventHandler(this.btnSpin_Click);
+            // 
+            // numCycles
+            // 
+            this.numCycles.Location = new System.Drawing.Point(342, 124);
+            this.numCycles.Name = "numCycles";
+            this.numCycles.Size = new System.Drawing.Size(120, 22);
+            this.numCycles.TabIndex = 1;
+            this.numCycles.ValueChanged += new System.EventHandler(this.numCycles_ValueChanged);
+            // 
+            // spinTimer
+            // 
+            this.spinTimer.Tick += new System.EventHandler(this.spinTimer_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(342, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Durata: (de cate ori se schimba imaginile)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numCycles);
+            this.Controls.Add(this.btnSpin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numCycles)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSpin;
+        private System.Windows.Forms.NumericUpDown numCycles;
+        private System.Windows.Forms.Timer spinTimer;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SlotMachineFinal/Form1.cs
@@ -0,0 +1,196 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace SlotMachineFinal
+{
+    // --- PARTEA 1: INTERFAȚA CU BUTOANE ---
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            if (numCycles != null)
+            {
+                numCycles.Minimum = 1;
+                numCycles.Value = 20;
+            }
+        }
+
+        private void btnSpin_Click(object sender, EventArgs e)
+        {
+            // Setări
+            int spins = 20; // Câte schimbări de imagine să facă
+            if (numCycles != null) spins = (int)numCycles.Value;
+            if (spins < 5) spins = 20; // Siguranță
+
+            this.Hide();
+
+            // Lansăm fereastra. Nu mai înmulțim cu 10 pentru că gestionăm durata altfel acum.
+            // Transmitem numărul de "schimbări" de imagine dorite.
+            using (SlotWindow game = new SlotWindow(spins))
+            {
+                game.Run(60.0); // 60 FPS
+
+                // Mesajul apare DUPĂ ce fereastra s-a închis (adică după ce ai văzut rezultatul)
+                if (game.Won)
+                    MessageBox.Show("JACKPOT! Ai câștigat!", "Rezultat");
+                else
+                    MessageBox.Show("Ai pierdut. Mai încearcă!", "Rezultat");
+            }
+
+            this.Show();
+        }
+
+        private void spinTimer_Tick(object sender, EventArgs e) { }
+        private void numCycles_ValueChanged(object sender, EventArgs e) { }
+    }
+
+    // --- PARTEA 2: FEREASTRA OPENGL (JOCUL) ---
+    class SlotWindow : GameWindow
+    {
+        int _spinsLeft;          // Câte schimbări de imagine au mai rămas
+        int _cooldown = 180;     // Timp de așteptare la final (180 cadre = 3 secunde)
+        int _frameSkipCounter = 0; // Contor pentru încetinire
+
+        int[] _slots = { 0, 0, 0 };
+        int[] _textureIds = new int[4];
+        Random _rand = new Random();
+        public bool Won = false;
+
+        public SlotWindow(int spins) : base(800, 300, GraphicsMode.Default, "Slot Machine - OpenGL")
+        {
+            _spinsLeft = spins;
+            this.VSync = VSyncMode.On;
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+            GL.ClearColor(Color.CornflowerBlue);
+            GL.Enable(EnableCap.Texture2D);
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.SrcAlpha, BlendingFactorDest.OneMinusSrcAlpha);
+
+            LoadTexture(0, "img0.png");
+            LoadTexture(1, "img1.png");
+            LoadTexture(2, "img2.png");
+            LoadTexture(3, "img3.png");
+
+            GL.MatrixMode(MatrixMode.Projection);
+            GL.LoadIdentity();
+            GL.Ortho(0, 300, 0, 100, -1, 1);
+            GL.MatrixMode(MatrixMode.Modelview);
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            // STADIUL 1: ÎNVÂRTIRE
+            if (_spinsLeft > 0)
+            {
+                _frameSkipCounter++;
+
+                // Schimbăm imaginile doar o dată la 5 cadre (încetinim animația)
+                if (_frameSkipCounter >= 5)
+                {
+                    _slots[0] = _rand.Next(0, 4);
+                    _slots[1] = _rand.Next(0, 4);
+                    _slots[2] = _rand.Next(0, 4);
+
+                    _spinsLeft--;       // Scădem un "spin"
+                    _frameSkipCounter = 0; // Resetăm contorul
+                }
+            }
+            // STADIUL 2: STOP ȘI AFIȘARE REZULTAT
+            else
+            {
+                // Prima dată când intrăm aici, verificăm dacă a câștigat
+                if (_cooldown == 180)
+                {
+                    if (_slots[0] == _slots[1] && _slots[1] == _slots[2])
+                        Won = true;
+                    else
+                        Won = false;
+                }
+
+                // Așteptăm 3 secunde (scădem din cooldown)
+                _cooldown--;
+
+                // Doar când trece timpul, închidem fereastra
+                if (_cooldown <= 0)
+                {
+                    this.Exit();
+                }
+            }
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            base.OnRenderFrame(e);
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            for (int i = 0; i < 3; i++)
+            {
+                DrawSlot(i, _slots[i]);
+            }
+
+            this.SwapBuffers();
+        }
+
+        private void DrawSlot(int pos, int texIndex)
+        {
+            if (_textureIds[texIndex] != 0)
+            {
+                GL.BindTexture(TextureTarget.Texture2D, _textureIds[texIndex]);
+                GL.Color3(Color.White);
+            }
+            else
+            {
+                GL.Disable(EnableCap.Texture2D);
+                // Culori fallback ca să vezi că se mișcă ceva
+                if (texIndex == 0) GL.Color3(Color.Red);
+                else if (texIndex == 1) GL.Color3(Color.Yellow);
+                else if (texIndex == 2) GL.Color3(Color.Green);
+                else GL.Color3(Color.Purple);
+            }
+
+            double xStart = pos * 100;
+            double xEnd = xStart + 100;
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.TexCoord2(0.0f, 1.0f); GL.Vertex2(xStart, 0);
+            GL.TexCoord2(1.0f, 1.0f); GL.Vertex2(xEnd, 0);
+            GL.TexCoord2(1.0f, 0.0f); GL.Vertex2(xEnd, 100);
+            GL.TexCoord2(0.0f, 0.0f); GL.Vertex2(xStart, 100);
+            GL.End();
+
+            GL.Enable(EnableCap.Texture2D);
+        }
+
+        private void LoadTexture(int index, string filename)
+        {
+            try
+            {
+                if (!System.IO.File.Exists(filename)) return;
+                _textureIds[index] = GL.GenTexture();
+                GL.BindTexture(TextureTarget.Texture2D, _textureIds[index]);
+                Bitmap bmp = new Bitmap(filename);
+                BitmapData data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                    ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                    data.Width, data.Height, 0,
+                    OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+                bmp.UnlockBits(data);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+                GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            }
+            catch { }
+        }
+    }
+}
